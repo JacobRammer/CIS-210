c72a5f8f7147cd3f0a43973e9ff2a2639af522f9
Jacob <jrammer101@gmail.com>
2019-01-16 17:18:41 -0800

    Assignment 2-1

---
+++ Flower/p23_drawflower.py
@@ -0,0 +1,17 @@
+"""
+Drawing with Python
+CIS 210 W19 Project 2-3
+
+Author: [Jacob Rammer]
+
+Credits: [N/A]
+
+Use python's Turtle module to draw
+various types of graphics
+"""
+import turtle  # remove before submit
+
+turtle.fd(1000)
+
+
+turtle.mainloop()  # remove before submit
