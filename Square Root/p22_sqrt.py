e92952e99042a6ea07ff62dea3e477edd0b096e7
Jacob <jrammer101@gmail.com>
2019-01-21 19:04:31 -0800

    Drew a flower in Turtle

--- Square Root/p22_sqrt.py
+++ Square Root/p22_sqrt.py
@@ -8,3 +8,29 @@ Credits: [N/A]
 
 # TODO desc
 """
+
+
+def main():
+    pass
+
+
+def mysqrt(n, k):
+    pass
+
+
+def sqrt_compare(num, iterations):
+    pass
+
+
+def test(n, k):
+    x1 = n / k
+    average = (k + x1) / 2
+
+    for i in range(k):
+        x2 = n / average
+        average2 = (average + x2) / 2
+
+    return average2
+
+
+print(test(25, 5))
