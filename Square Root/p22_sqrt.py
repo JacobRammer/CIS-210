ff371ca6adbb41fb801dcfe87d390995895ff73b
Jacob <jrammer101@gmail.com>
2019-01-21 20:45:26 -0800

    Found the square root using the Babylonian method

--- Square Root/p22_sqrt.py
+++ Square Root/p22_sqrt.py
@@ -8,29 +8,47 @@ Credits: [N/A]
 
 # TODO desc
 """
+from math import sqrt
 
 
 def main():
-    pass
+    '''Square root comparison program driver.'''
+
+    # sqrt_compare(25, 5)
+    # sqrt_compare(25, 10)
+    # sqrt_compare(625, 5)
+    # sqrt_compare(625, 10)
+    # sqrt_compare(10000, 8)
+    # sqrt_compare(10000, 10)
+    # sqrt_compare(10000, 11)
+
+    return None
 
 
 def mysqrt(n, k):
-    pass
+    x1 = n
+
+    for i in range(k):
+        average = n / x1
+        x1 = (x1 + average) / 2
+
+    return x1
 
 
 def sqrt_compare(num, iterations):
-    pass
+
+    print("For", num, "using", iterations, "iterations:")
+
+    mysqrt_value = mysqrt(num, iterations)
+    print("mysqrt value is: ", mysqrt_value)
+
+    lib_val = sqrt(num)
+    print("math lib val is: ", lib_val)
+
+    error_val = round(((mysqrt_value - lib_val) / lib_val) * 100, 2)
+    print("This is a:", error_val, "percent error")
+
+    return None
 
 
-def test(n, k):
-    x1 = n / k
-    average = (k + x1) / 2
-
-    for i in range(k):
-        x2 = n / average
-        average2 = (average + x2) / 2
-
-    return average2
-
-
-print(test(25, 5))
+main()
