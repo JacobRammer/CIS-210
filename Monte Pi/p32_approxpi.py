471e05a7e2eec364da90b9be8ff9d9412c554d7e
Jacob <jrammer101@gmail.com>
2019-01-23 15:50:05 -0800

    Started 3-2

---
+++ Monte Pi/p32_approxpi.py
@@ -0,0 +1,19 @@
+"""
+Monte Pi
+CIS 210 W19 Project 3-2
+
+Author: [Jacob Rammer]
+
+Credits: [N/A]
+
+Approximate the value of pi using the
+Monte Carlo algorithm.
+"""
+
+
+def montePi(numDarts):
+    pass
+
+
+def isInCircle(x, y, r):
+    pass
