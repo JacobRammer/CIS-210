a9d5f46b7916a84ad06e8b005b05027a7a036125
Jacob <jrammer101@gmail.com>
2019-01-24 18:18:26 -0800

    Started 3-2

--- Fizzbuzz/p31_fizzbuzz.py
+++ Fizzbuzz/p31_fizzbuzz.py
@@ -44,9 +44,9 @@ def fb(n):
 
         if (i % 3 == 0) and (i % 5 == 0):
             print("fizzbuzz")
-        elif (i % 3 == 0):
+        elif i % 3 == 0:
             print("fizz")
-        elif (i % 5 == 0):
+        elif i % 5 == 0:
             print("buzz")
         else:
             print(i)
