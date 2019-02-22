1104e47db15dcc6474ea642f266d1159bc246f01
Jacob <jrammer101@gmail.com>
2019-02-21 20:47:09 -0800

    Majors Data

---
+++ Majors_data/p71_majorsct.py
@@ -0,0 +1,22 @@
+"""
+CIS 210 Majors
+CIS 210 W19 Project #
+
+Author: [Jacob Rammer]
+
+Credits: [N/A]
+
+Analyze the different majors in CIS 210
+"""
+
+
+def majors_readf(fname):
+    pass
+
+
+def majors_analysis(majorsli):
+    pass
+
+
+def majors_report(majors_mode, majors_ct, majorsli):
+    pass
